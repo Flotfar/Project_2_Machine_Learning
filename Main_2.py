e7ddf25f1064ef7d28f02dd2c4eba4772dea635b
Flotfar <113911826+Flotfar@users.noreply.github.com>
2023-04-10 15:16:24 +0200

    Update Main_2.py

--- Main_2.py
+++ Main_2.py
@@ -4,13 +4,15 @@ import pandas as pd
 import time
 import torch
 from sklearn import model_selection
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import OneHotEncoder
-from toolbox_02450 import rlr_validate, train_neural_net, draw_neural_net
+from sklearn.linear_model import Ridge
+from toolbox_02450 import rlr_validate, train_neural_net
 from matplotlib import pyplot as plt
 from scipy.linalg import svd
-from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend, 
-                           title, subplot, show, grid)
+from matplotlib.pylab import figure, semilogx, loglog, xlabel, ylabel, legend, title, subplot, show, grid
 from tqdm import tqdm
+
 
 #############################################
 ####             Functions               ####
@@ -117,7 +119,7 @@ def PCA(data_values, one_k):
     plt.show()
 
 # Linear regression model
-def linear_regression(X, y, attributes, K, K_inner, lambdas):
+def linear_regression(X, y, attributes, K, lambdas):
     
     # Applying method from script 8.1.1
     # Adding offset attribute to X
@@ -141,17 +143,16 @@ def linear_regression(X, y, attributes, K, K_inner, lambdas):
         X_train = X_off[train_index]
         y_train = y[train_index]
 
-
-        ###### Inner fold, validation
-        opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, K_inner)
-        ######
-
+        # Calculating the optimal lambda value
+        opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, K)
+        
 
         mu[i, :] = np.mean(X_train[:, 1:], 0)
         sigma[i, :] = np.std(X_train[:, 1:], 0)
         
         X_train[:, 1:] = (X_train[:, 1:] - mu[i, :] ) / sigma[i, :] 
         
+        # Pre-calculate matrices
         Xty = X_train.T @ y_train
         XtX = X_train.T @ X_train
 
@@ -202,116 +203,260 @@ def linear_regression(X, y, attributes, K, K_inner, lambdas):
     for m in range(M):
         print('{:>15} {:>15}'.format(attributeNames[m+1], np.round(w_rlr[m+1,-1],3)))
 
-# Artificial neural network (ANN) model
-def ANN(X, y, K, K_inner, lambdas, h_units):
+
+# Defining ANN model for comparisment
+def ANN_model(M, hidden_units):
+    model = lambda: torch.nn.Sequential(
+                    torch.nn.Linear(M, hidden_units), #M features to H hiden units
+                    # 1st transfer function, either Tanh or ReLU:
+                    torch.nn.Tanh(),                            #torch.nn.ReLU(),
+                    torch.nn.Linear(hidden_units, 1), # H hidden units to 1 output neuron
+                    torch.nn.Sigmoid() # final tranfer function
+                    )
+    return model
+
+
+# Comparing models (Linear reg, ANN, baseline):
+def compare_models(X, y, K1, K2, lambdas, h_units):
 
     N, M = X.shape
 
-    # Create crossvalidation partition for evaluation
-    CV = model_selection.KFold(K, shuffle=True)
+    # Create crossvalidation partition for the loops
+    # rand.state = subtracting the same 'random' set every run
+    CV_out = model_selection.KFold(K1, shuffle=True, random_state=42)
+    CV_inn = model_selection.KFold(K2, shuffle=True, random_state=42)
     
     # Parameters for neural network classifier
     
-    n_replicates = 2        # number of networks trained in each k-fold
+    n_replicates = 1        # number of networks trained in each k-fold
     max_iter = 10000         # stop criterion 2 (max epochs in training)
     
     # Defining loss function
     loss_fn = torch.nn.BCELoss()
 
-    # Defining model
-    model = lambda: torch.nn.Sequential(
-                    torch.nn.Linear(M, h_units), #M features to H hiden units
-                    # 1st transfer function, either Tanh or ReLU:
-                    torch.nn.Tanh(),                            #torch.nn.ReLU(),
-                    torch.nn.Linear(h_units, 1), # H hidden units to 1 output neuron
-                    torch.nn.Sigmoid() # final tranfer function
-                    )
+    # Creating empty result array [0]K1-Fold, [1-2]RLR MSE, [3-4]ANN MSE, [5]Baseline MSE:
+    results = np.zeros((K1, 6))
 
-    print('Initiating training of model of type:\n{}\n'.format(str(model())))
+
+    # print('Initiating training of model of type:\n{}\n'.format(str(model())))
     start_time = time.time()
 
-    # Starting up the training loop:
-    # Initiating outer loop
-    for i, (train_index, test_index) in tqdm(enumerate(CV.split(X,y))): 
-        print('\n Outer fold: {0}/{1}'.format(i+1,K))
+    ###################################
+    # Starting up the training:
+    ###################################
 
+    #### Initiating outer loop ####
+    for i, (train_index, test_index) in enumerate(CV_out.split(X,y)): 
+        print('\n Outer fold: {0}/{1}'.format(i+1,K1))
 
         # Fetch appropriate training and test data
         X_train, y_train, X_test, y_test = X[train_index], y[train_index], X[test_index], y[test_index]
+        
 
-        # Preallocate memory to hold error terms
-        w = np.empty((M,K_inner,len(lambdas)))
-        rlr_test_error = np.empty((K_inner,len(lambdas)))
-        h_test_error = np.empty((K_inner,len(h_units)))
+        # Preallocate memory to hold error terms for baseline "w", linear reg "lr"
+        w = np.empty((M,K2,len(lambdas)))
+        rlr_test_error = np.empty((K2,len(lambdas)))
+        h_test_error = np.empty((K2,len(h_units)))
         errors = []
+        
+        opt_lambda = 0
+        best_lambda_mse = float('inf')
 
-        # Initiating inner loop
-        for i2, (train_index_inn, test_index_inn) in enumerate(CV.split(X,y)): 
-            print('\n Inner fold: {0}/{1}'.format(i2+1,K))
+        #### Initiating inner loop ####
+        for i2, (train_index_inn, test_index_inn) in enumerate(CV_inn.split(X_train,y_train)): 
+            print('\n Inner fold: {0}/{1}'.format(i2+1,K2))
 
             # Fetch appropriate training and test data
             X_train_inn, y_train_inn, X_test_inn, y_test_inn = X_train[train_index_inn], y_train[train_index_inn], X_train[test_index_inn], y_train[test_index_inn]
-
 
             # Converting testdata to PyTorch tensors
             X_train_tensor = torch.Tensor(X_train_inn)
             y_train_tensor = torch.Tensor(np.reshape(y_train_inn, (len(y_train_inn), -1)))
             X_test_tensor = torch.Tensor(X_test_inn)
-            y_test = torch.Tensor(y_test_inn)
 
             # Pre-calculate matrices
             Xty_inn = X_train_inn.T @ y_train_inn
             XtX_inn = X_train_inn.T @ X_train_inn
             
-            for i3, lambda_n in enumerate(lambdas):
-                # Estimating the optimal lamba value
-                lambdaI = lambda_n * np.eye(M)
+            # Calculating optimal lambda for regularized linear regression:
+            # for i3, lambda_n in enumerate(lambdas):
+            #     # Estimating the optimal lamba value
+            #     lambdaI = lambda_n * np.eye(M)
                 
-                # Calculate weights
-                w[:,i2,i3] = np.linalg.solve(XtX_inn+lambdaI,Xty_inn).squeeze()
+            #     # Calculate weights
+            #     w[:,i2,i3] = np.linalg.solve(XtX_inn+lambdaI, Xty_inn).squeeze()
 
-                # Insert Mean Squared Error into the array
-                rlr_test_error[i2,i3] = np.square(y_test_inn-X_test_inn @ w[:,i2,i3].T).mean(axis=0)
+            #     # Insert Mean Squared Error into the array
+            #     rlr_test_error[i2,i3] = np.square(y_test_inn-X_test_inn @ w[:,i2,i3].T).mean(axis=0)
+
+            #### Training RLR model(s) ####
+            mse_l = []
+            for i3, lambda_n in enumerate(lambdas):
+                rlr = Ridge(alpha=lambda_n)
+                rlr.fit(X_train_inn, y_train_inn)
+                y_pred_rlr = rlr.predict(X_test_inn)
+                mse_l.append(((y_test_inn - y_pred_rlr)**2).mean())
+            
+            # Determining the optimal lambda value
+            mean_mse_l = np.mean(mse_l)
+
+            if mean_mse_l < best_lambda_mse:
+                opt_lambda = lambda_n
+                opt_lambda_mse = mean_mse_l
 
 
-            ###################################
-            # Det er ret sketchy her fra (som i kan se har jeg været inspireret af kaspers model, ved ikke om det er for meget though)
+            #### Training ANN model(s) ####
+            for i4, h in enumerate(h_units):
+                # Choosing ANN by h_unit
+                model = ANN_model(M, h)
 
-            # Training ANN
-            net, final_loss, learning_curve = train_neural_net(model,
-                                                       loss_fn,
-                                                       X=X_train,
-                                                       y=y_train,
-                                                       n_replicates=3,
-                                                       max_iter=max_iter)
+                # Training the ANN
+                net, final_loss, learning_curve = train_neural_net(model, loss_fn, X=X_train_tensor, y=y_train_tensor, n_replicates=n_replicates, max_iter=max_iter)
+
+                # Calculate y prediction and convert it to np array
+                y_test_est = net(X_test_tensor)
+                y_test_est_np = y_test_est.cpu().detach().numpy() 
+
+                # Insert MSE into the array
+                h_test_error[i2, i4] =  np.square(y_test_est_np - y_test_inn.T).sum(axis=0)[0] / y_test_inn.size
+
+
+        # Convert np arrays to tensors
+        X_train_tensor = torch.Tensor(X_train)
+        y_train_tensor = torch.Tensor(np.reshape(y_train, (len(y_train), -1)))
+        X_test_tensor = torch.Tensor(X_test)
+
+
+        #### Computing the ANN with optimal h-units ####
+        #(ANN) Calculate the optimal amount of hidden units
+        opt_h = h_units[np.argmin(np.mean(h_test_error,axis=0))]
+        
+        #(ANN) creating ANN model with optimal 'opt_h'
+        model = ANN_model(M, opt_h)
+
+        #(ANN) Training the opt_ANN:
+        net, final_loss, learning_curve = train_neural_net(model, loss_fn, X=X_train_tensor, y=y_train_tensor, n_replicates=n_replicates)
+
+        #(ANN) Calculate y prediction and convert it to np array
+        y_test_est = net(X_test_tensor)
+        y_test_est_np = y_test_est.cpu().detach().numpy() 
+
+        #(ANN) Calculating the opt_h MSE
+        opt_h_mse =  np.square(y_test_est_np.flatten() - y_test.T).mean(axis=0)
+        ################################################
+
+
+        #### Computing the RLR with optimal lambda-value ####
+        # #(RLR) Collecting the optimal lambda choice
+        # opt_lambda = lambdas[np.argmin(np.mean(rlr_test_error,axis=0))]
+
+        # #(RLR) Pre-calculate matrices
+        # Xty = X_train.T @ y_train
+        # XtX = X_train.T @ X_train
+
+        # #(RLR) calculating the weights
+        # lambdaI = opt_lambda * np.eye(M)
+        # w_rlr = np.linalg.solve(XtX+lambdaI, Xty).squeeze()
+
+        # #(RLR) Calculating the MSE for the optimal lambda value
+        # opt_lambda_mse = np.square(y_test-X_test @ w_rlr).sum(axis=0)/y_test.shape[0]
+        ################################################
+
+
+        #### Computing the LR (Baseline) ####
+        # Training a LR without regularization i.e alpha=0
+        baseline = Ridge(alpha=0)
+        baseline.fit(X_train, y_train)
+        y_pred_baseline = baseline.predict(X_test)
+
+        #(LR) Calculate MSE for baseline model
+        lr_mse = ((y_test - y_pred_baseline)**2).mean()
+        ################################################
+
+
+        # Record the results
+        results[i, 0] = i+1
+        results[i, 1] = opt_h
+        results[i, 2] = opt_h_mse
+        results[i, 3] = opt_lambda
+        results[i, 4] = opt_lambda_mse
+        results[i, 5] = lr_mse
     
-            print('\n\tBest loss: {}\n'.format(final_loss))
 
-            # Determine estimated class labels for test set
-            y_sigmoid = net(X_test) # activation of final note, i.e. prediction of network
-            y_test_est = (y_sigmoid > .5).type(dtype=torch.uint8) # threshold output of sigmoidal function
-            y_test = y_test.type(dtype=torch.uint8)
-            # Determine errors and error rate
-            e = (y_test_est != y_test)
-            error_rate = (sum(e).type(torch.float)/len(y_test)).data.numpy()
-            errors.append(error_rate) # store error rate for current CV fold
-
-    
-
-    
-    
-    
-    
     end_time = time.time()
 
     runtime = end_time - start_time
     print("Runtime:", runtime, "seconds")
+    print("\n Results:")
+    print(results)
+
+
+'''
+def compare_models(X, y, K1, K2, lambdas, h_values):
+
+    kf_outer = KFold(n_splits=K1, shuffle=True, random_state=42)
+    kf_inner = KFold(n_splits=K2, shuffle=True, random_state=42)
     
+    results = np.zeros((K1, 6))
 
 
+    for i, (train_index, test_index) in enumerate(kf_outer.split(X,y)):
+        X_train_outer, X_test_outer = X[train_index], X[test_index]
+        y_train_outer, y_test_outer = y[train_index], y[test_index]
 
+        best_h = 0
+        best_h_mse = float('inf')
+        best_lambda = 0
+        best_lambda_mse = float('inf')
 
+        for h in h_values:
+            for l in lambdas:
+                mse_h = []
+                mse_l = []
+                
+                for train_inner_index, test_inner_index in kf_inner.split(X_train_outer):
+                    X_train_inner, X_test_inner = X_train_outer[train_inner_index], X_train_outer[test_inner_index]
+                    y_train_inner, y_test_inner = y_train_outer[train_inner_index], y_train_outer[test_inner_index]
 
+                    # Train ANN
+                    ann = MLPRegressor(hidden_layer_sizes=(h,), max_iter=1000, alpha=l, solver='adam',
+                                        activation='relu', random_state=42)
+                    ann.fit(X_train_inner, y_train_inner)
+                    y_pred_ann = ann.predict(X_test_inner)
+                    mse_h.append(((y_test_inner - y_pred_ann)**2).mean())
+
+                    # Train RLR
+                    rlr = Ridge(alpha=l)
+                    rlr.fit(X_train_inner, y_train_inner)
+                    y_pred_rlr = rlr.predict(X_test_inner)
+                    mse_l.append(((y_test_inner - y_pred_rlr)**2).mean())
+
+                mean_mse_h = np.mean(mse_h)
+                mean_mse_l = np.mean(mse_l)
+
+                # Update best h and lambda
+                if mean_mse_h < best_h_mse:
+                    best_h = h
+                    best_h_mse = mean_mse_h
+                if mean_mse_l < best_lambda_mse:
+                    best_lambda = l
+                    best_lambda_mse = mean_mse_l
+
+        # Train baseline model
+        baseline = Ridge(alpha=0)
+        baseline.fit(X_train_outer, y_train_outer)
+        y_pred_baseline = baseline.predict(X_test_outer)
+
+        # Record results
+        results[i, 0] = i+1
+        results[i, 1] = best_h
+        results[i, 2] = best_h_mse
+        results[i, 3] = best_lambda
+        results[i, 4] = best_lambda_mse
+        results[i, 5] = ((y_test_outer - y_pred_baseline)**2).mean()
+
+    return results
+'''
 
 
 #############################################
@@ -331,16 +476,11 @@ X, y, attributes, data_values, one_k = dataprep()
 K = 10
 K_inner = K
 lambdas = np.power(10.,range(-2,8))
-linear_regression(X, y, attributes, K, K_inner, lambdas)
+# linear_regression(X, y, attributes, K, lambdas)
 
-#### ANN model, part B:
-h_units = range(1,10)     # number of hidden units
-K = 10
-K_inner = K
-lambdas = range(1,101)
-
-#### Linear regression model, part A:
-K = 10
-K_inner = K
-lambdas = np.power(10.,range(-1,8))
-linear_regression(X, y, attributes, K, K_inner, lambdas)
+#### Comparing models, part B:
+h_units = range(1,11)     # number of hidden units
+K1 = 10
+K2 = K1
+lambdas = range(1,50)
+compare_models(X, y, K1, K2, lambdas, h_units)
