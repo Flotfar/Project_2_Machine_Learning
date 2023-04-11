c99b0f3337201d18791aa8b1780ef8c1d9540674
Flotfar <113911826+Flotfar@users.noreply.github.com>
2023-04-11 15:08:58 +0200

    Update Main_2.py

--- Main_2.py
+++ Main_2.py
@@ -4,14 +4,13 @@ import pandas as pd
 import time
 import torch
 from sklearn import model_selection
-from sklearn.model_selection import KFold
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, LinearRegression
 from toolbox_02450 import rlr_validate, train_neural_net
 from matplotlib import pyplot as plt
 from scipy.linalg import svd
 from matplotlib.pylab import figure, semilogx, loglog, xlabel, ylabel, legend, title, subplot, show, grid
-from tqdm import tqdm
+from tabulate import tabulate
 
 
 #############################################
@@ -55,10 +54,7 @@ def dataprep ():
     one_k = encoder.fit_transform(seasons.reshape(-1, 1))
     X = np.append(standard_X, one_k, axis=1)
     # exporting the season category array
-    season_categories = encoder.categories_[0]
-
-    # Seasons = np.array([get_season(day) for day in data_values[0:, 9]], copy=False, subok=True, ndmin=2).T
-    
+    season_categories = encoder.categories_[0]    
 
     #Extending attributes with seasons:
     label = list(attributes)
@@ -68,8 +64,6 @@ def dataprep ():
     # Creating Y column
     Y_raw = data_values[:, 0]
     Y = (Y_raw - np.mean(Y_raw))/np.std(Y_raw)
-
-    # .to_numpy()
 
     print("Data loaded succesfully. \n")
     return X, Y, attributes, data_values, one_k
@@ -203,7 +197,6 @@ def linear_regression(X, y, attributes, K, lambdas):
     for m in range(M):
         print('{:>15} {:>15}'.format(attributeNames[m+1], np.round(w_rlr[m+1,-1],3)))
 
-
 # Defining ANN model for comparisment
 def ANN_model(M, hidden_units):
     model = lambda: torch.nn.Sequential(
@@ -214,7 +207,6 @@ def ANN_model(M, hidden_units):
                     torch.nn.Sigmoid() # final tranfer function
                     )
     return model
-
 
 # Comparing models (Linear reg, ANN, baseline):
 def compare_models(X, y, K1, K2, lambdas, h_units):
@@ -236,7 +228,7 @@ def compare_models(X, y, K1, K2, lambdas, h_units):
 
     # Creating empty result array [0]K1-Fold, [1-2]RLR MSE, [3-4]ANN MSE, [5]Baseline MSE:
     results = np.zeros((K1, 6))
-
+    results_label = np.array(["Fold", "h-val", "h-MSE", "l-val", "l-MSE", "b-MSE"])
 
     # print('Initiating training of model of type:\n{}\n'.format(str(model())))
     start_time = time.time()
@@ -252,15 +244,10 @@ def compare_models(X, y, K1, K2, lambdas, h_units):
         # Fetch appropriate training and test data
         X_train, y_train, X_test, y_test = X[train_index], y[train_index], X[test_index], y[test_index]
         
-
         # Preallocate memory to hold error terms for baseline "w", linear reg "lr"
-        w = np.empty((M,K2,len(lambdas)))
-        rlr_test_error = np.empty((K2,len(lambdas)))
         h_test_error = np.empty((K2,len(h_units)))
-        errors = []
-        
         opt_lambda = 0
-        best_lambda_mse = float('inf')
+        opt_lambda_mse = float('inf')
 
         #### Initiating inner loop ####
         for i2, (train_index_inn, test_index_inn) in enumerate(CV_inn.split(X_train,y_train)): 
@@ -277,33 +264,22 @@ def compare_models(X, y, K1, K2, lambdas, h_units):
             # Pre-calculate matrices
             Xty_inn = X_train_inn.T @ y_train_inn
             XtX_inn = X_train_inn.T @ X_train_inn
-            
-            # Calculating optimal lambda for regularized linear regression:
-            # for i3, lambda_n in enumerate(lambdas):
-            #     # Estimating the optimal lamba value
-            #     lambdaI = lambda_n * np.eye(M)
-                
-            #     # Calculate weights
-            #     w[:,i2,i3] = np.linalg.solve(XtX_inn+lambdaI, Xty_inn).squeeze()
 
-            #     # Insert Mean Squared Error into the array
-            #     rlr_test_error[i2,i3] = np.square(y_test_inn-X_test_inn @ w[:,i2,i3].T).mean(axis=0)
 
-            #### Training RLR model(s) ####
+            #### Training RLR model ####
             mse_l = []
-            for i3, lambda_n in enumerate(lambdas):
+            for lambda_n in lambdas:
                 rlr = Ridge(alpha=lambda_n)
                 rlr.fit(X_train_inn, y_train_inn)
                 y_pred_rlr = rlr.predict(X_test_inn)
                 mse_l.append(((y_test_inn - y_pred_rlr)**2).mean())
+
+                # Determining the optimal lambda value
+                mean_mse_l = np.mean(mse_l)
             
-            # Determining the optimal lambda value
-            mean_mse_l = np.mean(mse_l)
-
-            if mean_mse_l < best_lambda_mse:
-                opt_lambda = lambda_n
-                opt_lambda_mse = mean_mse_l
-
+                if mean_mse_l < opt_lambda_mse:
+                    opt_lambda = lambda_n
+                    opt_lambda_mse = mean_mse_l
 
             #### Training ANN model(s) ####
             for i4, h in enumerate(h_units):
@@ -346,26 +322,9 @@ def compare_models(X, y, K1, K2, lambdas, h_units):
         ################################################
 
 
-        #### Computing the RLR with optimal lambda-value ####
-        # #(RLR) Collecting the optimal lambda choice
-        # opt_lambda = lambdas[np.argmin(np.mean(rlr_test_error,axis=0))]
-
-        # #(RLR) Pre-calculate matrices
-        # Xty = X_train.T @ y_train
-        # XtX = X_train.T @ X_train
-
-        # #(RLR) calculating the weights
-        # lambdaI = opt_lambda * np.eye(M)
-        # w_rlr = np.linalg.solve(XtX+lambdaI, Xty).squeeze()
-
-        # #(RLR) Calculating the MSE for the optimal lambda value
-        # opt_lambda_mse = np.square(y_test-X_test @ w_rlr).sum(axis=0)/y_test.shape[0]
-        ################################################
-
-
         #### Computing the LR (Baseline) ####
         # Training a LR without regularization i.e alpha=0
-        baseline = Ridge(alpha=0)
+        baseline = LinearRegression()
         baseline.fit(X_train, y_train)
         y_pred_baseline = baseline.predict(X_test)
 
@@ -377,86 +336,26 @@ def compare_models(X, y, K1, K2, lambdas, h_units):
         # Record the results
         results[i, 0] = i+1
         results[i, 1] = opt_h
-        results[i, 2] = opt_h_mse
+        results[i, 2] = round(opt_h_mse, 3)
         results[i, 3] = opt_lambda
-        results[i, 4] = opt_lambda_mse
-        results[i, 5] = lr_mse
+        results[i, 4] = round(opt_lambda_mse, 3)
+        results[i, 5] = round(lr_mse, 3)
     
 
-    end_time = time.time()
+    # creating titles for the result output and
+    # printing the array as a LaTeX table using tabulate
+    results_w_labels = np.row_stack((results_label, results))
+    result_table = tabulate(results_w_labels, headers='firstrow', tablefmt='latex_booktabs')
 
+    # defining runtime
+    end_time = time.time()
     runtime = end_time - start_time
+
+    # printing results
     print("Runtime:", runtime, "seconds")
     print("\n Results:")
-    print(results)
+    print(result_table)
 
-
-'''
-def compare_models(X, y, K1, K2, lambdas, h_values):
-
-    kf_outer = KFold(n_splits=K1, shuffle=True, random_state=42)
-    kf_inner = KFold(n_splits=K2, shuffle=True, random_state=42)
-    
-    results = np.zeros((K1, 6))
-
-
-    for i, (train_index, test_index) in enumerate(kf_outer.split(X,y)):
-        X_train_outer, X_test_outer = X[train_index], X[test_index]
-        y_train_outer, y_test_outer = y[train_index], y[test_index]
-
-        best_h = 0
-        best_h_mse = float('inf')
-        best_lambda = 0
-        best_lambda_mse = float('inf')
-
-        for h in h_values:
-            for l in lambdas:
-                mse_h = []
-                mse_l = []
-                
-                for train_inner_index, test_inner_index in kf_inner.split(X_train_outer):
-                    X_train_inner, X_test_inner = X_train_outer[train_inner_index], X_train_outer[test_inner_index]
-                    y_train_inner, y_test_inner = y_train_outer[train_inner_index], y_train_outer[test_inner_index]
-
-                    # Train ANN
-                    ann = MLPRegressor(hidden_layer_sizes=(h,), max_iter=1000, alpha=l, solver='adam',
-                                        activation='relu', random_state=42)
-                    ann.fit(X_train_inner, y_train_inner)
-                    y_pred_ann = ann.predict(X_test_inner)
-                    mse_h.append(((y_test_inner - y_pred_ann)**2).mean())
-
-                    # Train RLR
-                    rlr = Ridge(alpha=l)
-                    rlr.fit(X_train_inner, y_train_inner)
-                    y_pred_rlr = rlr.predict(X_test_inner)
-                    mse_l.append(((y_test_inner - y_pred_rlr)**2).mean())
-
-                mean_mse_h = np.mean(mse_h)
-                mean_mse_l = np.mean(mse_l)
-
-                # Update best h and lambda
-                if mean_mse_h < best_h_mse:
-                    best_h = h
-                    best_h_mse = mean_mse_h
-                if mean_mse_l < best_lambda_mse:
-                    best_lambda = l
-                    best_lambda_mse = mean_mse_l
-
-        # Train baseline model
-        baseline = Ridge(alpha=0)
-        baseline.fit(X_train_outer, y_train_outer)
-        y_pred_baseline = baseline.predict(X_test_outer)
-
-        # Record results
-        results[i, 0] = i+1
-        results[i, 1] = best_h
-        results[i, 2] = best_h_mse
-        results[i, 3] = best_lambda
-        results[i, 4] = best_lambda_mse
-        results[i, 5] = ((y_test_outer - y_pred_baseline)**2).mean()
-
-    return results
-'''
 
 
 #############################################
@@ -481,6 +380,6 @@ lambdas = np.power(10.,range(-2,8))
 #### Comparing models, part B:
 h_units = range(1,11)     # number of hidden units
 K1 = 10
-K2 = K1
-lambdas = range(1,50)
+K2 = 10
+lambdas = range(1,100)
 compare_models(X, y, K1, K2, lambdas, h_units)
